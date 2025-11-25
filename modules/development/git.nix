67b857afb0aa20d09e4775193a9af90c16d7d1db
Kieran Beals <kieranbeals@gmail.com>
2025-11-24 23:20:11 -0500

    Fixed email & name in git.nix

--- modules/development/git.nix
+++ modules/development/git.nix
@@ -7,35 +7,36 @@ in
     programs = {
       git = {
         enable = true;
-        userEmail = {
-          inherit (meta.users.kieran) email;
-        };
-        userName = {
-          inherit (meta.users.kieran) name;
-        };
-        aliases = {
-          a = "add";
-          b = "branch";
-          c = "commit";
-          ca = "commit --amend";
-          cm = "commit -m";
-          co = "checkout";
-          d = "diff";
-          ds = "diff --staged";
-          l = "log --oneline";
-          ll = "log";
-          p = "push";
-          pf = "push --force-with-lease";
-          pl = "pull";
-          r = "rebase";
-          s = "status --short";
-          ss = "status";
-          sw = "switch";
-          # W alias by Karun
-          forgor = "commit --amend --no-edit";
-          graph = "log --all --decorate --graph --oneline";
-          oops = "checkout --";
-        };
+				settings = {
+          user = {
+            inherit (meta.users.kieran) email;
+            inherit (meta.users.kieran) name;
+          };
+
+          alias = {
+            a = "add";
+            b = "branch";
+            c = "commit";
+            ca = "commit --amend";
+            cm = "commit -m";
+            co = "checkout";
+            d = "diff";
+            ds = "diff --staged";
+            l = "log --oneline";
+            ll = "log";
+            p = "push";
+            pf = "push --force-with-lease";
+            pl = "pull";
+            r = "rebase";
+            s = "status --short";
+            ss = "status";
+            sw = "switch";
+            # W alias by Karun
+            forgor = "commit --amend --no-edit";
+            graph = "log --all --decorate --graph --oneline";
+            oops = "checkout --";
+          };
+				};
       };
 
       # Syntax-highlighting
