4b6ea708e0e9df671aa50ec2b2eb7b76feea6e65
Kieran Beals <kieranbeals@gmail.com>
2025-11-29 03:24:12 -0500

    Update power.nix

--- modules/laptop/power.nix
+++ modules/laptop/power.nix
@@ -2,8 +2,15 @@
   flake.modules.nixos.laptop = {
     services.tlp = {
       enable = true;
+      # Settings taken from: https://linrunner.de/tlp/support/optimizing.html#extend-battery-runtime
       settings = {
-
+        CPU_ENERGY_PERF_POLICY_ON_BAT="power";
+        PLATFORM_PROFILE_ON_BAT="low-power";
+        CPU_BOOST_ON_BAT="0";
+        CPU_BOOST_ON_SAV="0";
+        CPU_HWP_DYN_BOOST_ON_BAT="0";
+        CPU_HWP_DYN_BOOST_ON_SAV="0";
+        AMDGPU_ABM_LEVEL_ON_BAT="3";
       };
     };
 	};
