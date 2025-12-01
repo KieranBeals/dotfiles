e965f6a9ebecade474f936b7d3c683b1424f1185
Kieran Beals <kieranbeals@gmail.com>
2025-12-01 00:50:28 -0500

    Added swap

--- modules/hosts/hephaestus/hardware-configuration.nix
+++ modules/hosts/hephaestus/hardware-configuration.nix
@@ -1,5 +1,5 @@
 {
-	flake.modules.nixos."hosts/hephaestus" = 
+	flake.modules.nixos."hosts/hephaestus" =
   { config, lib, modulesPath, ... }:
   {
     imports =
@@ -8,7 +8,7 @@
 
     networking.networkmanager.enable = true;
     networking.networkmanager.wifi.powersave = false; # EDUROAM :)))
-	 
+
 	  boot.loader.systemd-boot.enable = true;
 		boot.loader.efi.canTouchEfiVariables = true;
 		boot.loader.efi.efiSysMountPoint = "/boot";
@@ -27,22 +27,27 @@
       { device = "/dev/disk/by-uuid/bd8016da-ce3c-46f1-90b1-ebf572e0d490";
         fsType = "ext4";
       };
-  
+
     fileSystems."/boot" =
       { device = "/dev/disk/by-uuid/2846-6FF8";
         fsType = "vfat";
         options = [ "fmask=0077" "dmask=0077" ];
       };
-  
-    swapDevices = [ ];
-  
+
+    swapDevices = [
+      {
+        device = "/swapfile";
+        size = 100 * 1024;
+      }
+    ];
+
     # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
     # (the default) this is the recommended approach. When using systemd-networkd it's
     # still possible to use this option, but it's recommended to use it in conjunction
     # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
     networking.useDHCP = lib.mkDefault true;
     # networking.interfaces.wlp192s0.useDHCP = lib.mkDefault true;
-  
+
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
