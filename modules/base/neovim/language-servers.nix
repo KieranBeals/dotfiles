27c8fbefccb2abe847b8cead92850cad307bf050
Kieran Beals <kieranbeals@gmail.com>
2026-01-02 12:03:26 -0800

    Update language-servers.nix

--- modules/base/neovim/language-servers.nix
+++ modules/base/neovim/language-servers.nix
@@ -1,10 +1,11 @@
 {
-  flake.modules.nixos.nixos = 
-	{ pkgs, ... }: 
+  flake.modules.nixos.nixos =
+	{ pkgs, ... }:
 	{
 	  environment.systemPackages = with pkgs; [
 	    lua-language-server
 			rust-analyzer
+			cargo
 			nixd
 			java-language-server
 	  ];
