4a7b553de225ea1b10ebbb5dce7e0cfc90034815
Kieran Beals <kieranbeals@gmail.com>
2026-01-05 15:09:00 -0500

    plasma-manager

--- modules/desktop/plasma6.nix
+++ modules/desktop/plasma6.nix
@@ -1,8 +1,80 @@
+{ inputs, ... }:
 {
-  flake.modules.nixos.desktop = {
-    services.xserver.enable = true;
-    services.displayManager.sddm.enable = true;
-    services.displayManager.sddm.wayland.enable = true;
-    services.desktopManager.plasma6.enable = true;
+  flake.modules = {
+    nixos.desktop =
+      { pkgs, ... }:
+      {
+        services.xserver.enable = true;
+        services.displayManager.sddm.enable = true;
+        services.displayManager.sddm.wayland.enable = true;
+        services.desktopManager.plasma6.enable = true;
+
+        xdg.portal = {
+          enable = true;
+          xdgOpenUsePortal = true;
+
+          extraPortals = with pkgs.kdePackages; [
+            xdg-desktop-portal-kde # Qt 6 version for Plasma 6
+          ];
+
+          config = {
+            kde = {
+              default = [
+                "kde"
+                "gtk"
+              ];
+              "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
+              "org.freedesktop.impl.portal.Screenshot" = [ "kde" ];
+              "org.freedesktop.impl.portal.ScreenCast" = [ "kde" ];
+            };
+          };
+        };
+      };
+
+    homeManager.desktop =
+      { ... }:
+      {
+        imports = [
+          inputs.plasma-manager.homeModules.plasma-manager
+        ];
+
+        # Now you can use plasma-manager options
+        programs.plasma = {
+          enable = true;
+
+          # input = {
+          #   touchpad = {
+          #     enabled = true;
+          #     naturalScrolling = true;
+          #   };
+          # };
+
+          workspace = {
+            theme = "breeze-dark";
+            colorScheme = "BreezeDark";
+            # wallpaper = "/path/to/wallpaper.png";
+          };
+
+          panels = [
+            {
+              location = "bottom";
+              height = 44;
+              widgets = [
+                "org.kde.plasma.kickoff"
+                "org.kde.plasma.icontasks"
+                "org.kde.plasma.systemtray"
+                "org.kde.plasma.digitalclock"
+              ];
+            }
+          ];
+
+          shortcuts = {
+            "kwin"."Window Close" = "Meta+C";
+            "kwin"."Overview" = "Meta+Tab";
+          };
+
+          overrideConfig = true;
+        };
+      };
   };
 }
