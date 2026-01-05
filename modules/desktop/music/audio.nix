b2c67844c4a4d8dfa834bda548c7337c6bdd4bd0
Kieran Beals <kieranbeals@gmail.com>
2026-01-05 15:07:55 -0500

    Formatting

--- modules/desktop/music/audio.nix
+++ modules/desktop/music/audio.nix
@@ -1,13 +1,12 @@
 {
-  flake.modules.nixos.desktop = 
-	{
-	  services.pipewire = {
-		  enable = true;
-			alsa = {
-			  enable = true;
-			  support32Bit = true;
-			};
-			pulse.enable = true;
-		};
-  };  
+  flake.modules.nixos.desktop = {
+    services.pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+  };
 }
