5419a01b1292a8f339e02de7b47961852a734f06
Kieran Beals <kieranbeals@gmail.com>
2026-01-19 13:03:35 -0500

    removed swap

--- modules/hosts/hephaestus/hardware-configuration.nix
+++ modules/hosts/hephaestus/hardware-configuration.nix
@@ -1,58 +1,72 @@
 { inputs, ... }:
 {
-	flake.modules.nixos."hosts/hephaestus" =
-  { config, lib, modulesPath, ... }:
-  {
-    imports = [
-      (modulesPath + "/installer/scan/not-detected.nix")
-      inputs.nixos-hardware.nixosModules.framework-16-7040-amd
+  flake.modules.nixos."hosts/hephaestus" =
+    {
+      config,
+      lib,
+      modulesPath,
+      ...
+    }:
+    {
+      imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+        inputs.nixos-hardware.nixosModules.framework-16-7040-amd
       ];
 
-    hardware.bluetooth.enable = true;
+      hardware.bluetooth.enable = true;
 
-    networking.networkmanager.enable = true;
-    networking.networkmanager.wifi.powersave = false; # EDUROAM :)))
+      networking.networkmanager.enable = true;
+      networking.networkmanager.wifi.powersave = false; # EDUROAM :)))
 
-	  boot.loader.systemd-boot.enable = true;
-		boot.loader.efi.canTouchEfiVariables = true;
-		boot.loader.efi.efiSysMountPoint = "/boot";
-    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-amd"
-    "amd_pstate" ];
-    boot.kernelParams = [
-      "amd_pstate=active"
-      "amd_pstate.shared_mem=1"
-      "mem_sleep_default=s2idle"
-    ];
-    boot.extraModulePackages = [ ];
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.efi.efiSysMountPoint = "/boot";
+      boot.initrd.availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "thunderbolt"
+      ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [
+        "kvm-amd"
+        "amd_pstate"
+      ];
+      boot.kernelParams = [
+        "amd_pstate=active"
+        "amd_pstate.shared_mem=1"
+        "mem_sleep_default=s2idle"
+      ];
+      boot.extraModulePackages = [ ];
 
-    fileSystems."/" =
-      { device = "/dev/disk/by-uuid/bd8016da-ce3c-46f1-90b1-ebf572e0d490";
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/bd8016da-ce3c-46f1-90b1-ebf572e0d490";
         fsType = "ext4";
       };
 
-    fileSystems."/boot" =
-      { device = "/dev/disk/by-uuid/2846-6FF8";
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/2846-6FF8";
         fsType = "vfat";
-        options = [ "fmask=0077" "dmask=0077" ];
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
       };
 
-    swapDevices = [
-      {
-        device = "/swapfile";
-        size = 100 * 1024;
-      }
-    ];
+      # swapDevices = [
+      #  {
+      #    device = "/swapfile";
+      #    size = 100 * 1024;
+      #  }
+      # ];
 
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    networking.useDHCP = lib.mkDefault true;
-    # networking.interfaces.wlp192s0.useDHCP = lib.mkDefault true;
+      # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+      # (the default) this is the recommended approach. When using systemd-networkd it's
+      # still possible to use this option, but it's recommended to use it in conjunction
+      # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+      networking.useDHCP = lib.mkDefault true;
+      # networking.interfaces.wlp192s0.useDHCP = lib.mkDefault true;
 
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  };
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    };
 }
