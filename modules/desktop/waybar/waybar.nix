b4f433c39121b448ae88d5e3af5d828e4009c3b6
Kieran Beals <kieranbeals@gmail.com>
2026-01-30 14:55:26 -0500

    Create waybar.nix

---
+++ modules/desktop/waybar/waybar.nix
@@ -0,0 +1,18 @@
+{
+  flake.modules = {
+	  nixos.desktop = 
+	    { pkgs, ... }:
+      {
+        environment.systemPackages = with pkgs; [
+	    	  waybar
+        ];
+      };
+
+    # homeManager.desktop = {
+    #   home.file.".config/waybar" = {
+    #     source = ./config;
+    #     recursive = true;
+    #   };
+	  # };
+	};
+}
