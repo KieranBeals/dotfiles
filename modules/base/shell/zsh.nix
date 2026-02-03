4491f4ed3456afbc611701fb8cbde95aeadba130
Kieran Beals <kieranbeals@gmail.com>
2026-02-03 16:25:10 -0500

    Added zsh plugins

--- modules/base/shell/zsh.nix
+++ modules/base/shell/zsh.nix
@@ -1,5 +1,10 @@
 {
-  flake.modules.nixos.desktop = {
-    programs.zsh.enable = true;
+  flake.modules.nixos.desktop =	{
+    programs.zsh = {
+		  enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+    };
   };
 }
