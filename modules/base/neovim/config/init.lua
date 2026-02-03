8ba5e52c12b1eab7e018682fff884a8790cdcf6a
Kieran Beals <kieranbeals@gmail.com>
2026-02-03 16:23:35 -0500

    tabs are now 2 spaces

--- modules/base/neovim/config/init.lua
+++ modules/base/neovim/config/init.lua
@@ -1,7 +1,10 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
+vim.o.expandtab = true
 vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
 vim.o.swapfile = false
 vim.g.mapleader = " "
 vim.o.undofile = true
