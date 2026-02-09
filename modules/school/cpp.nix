8949c823c5bc7e5a1c8a1accc89f115b7d789e9d
Kieran Beals <kieranbeals@gmail.com>
2026-02-09 15:23:55 -0500

    Update cpp.nix

--- modules/school/cpp.nix
+++ modules/school/cpp.nix
@@ -1,9 +1,25 @@
 {
-  flake.modules.homeManager.school =
-    { pkgs, ... }:
-    {
-      home.packages = with pkgs; [
-        rocmPackages.clang
-      ];
-    };
+  flake.modules = {
+    nixos.school =
+      { pkgs, ... }:
+      {
+        programs.vscode = {
+          enable = true;
+          extensions = with pkgs.vscode-extensions; [
+            vscodevim.vim
+            ms-vscode-remote.remote-ssh
+            ms-vscode.remote-explorer
+            ms-vscode.cpptools-extension-pack
+          ];
+        };
+      };
+    homeManager.school =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          rocmPackages.clang
+          vscode
+        ];
+      };
+  };
 }
