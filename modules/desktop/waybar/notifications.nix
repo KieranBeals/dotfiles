84d6bfe38c610d24cf756c7b4d0066a125bb1891
Kieran Beals <kieranbeals@gmail.com>
2026-02-13 22:38:53 -0500

    auto start notification center

--- modules/desktop/waybar/notifications.nix
+++ modules/desktop/waybar/notifications.nix
@@ -1,9 +1,16 @@
 {
-  flake.modules.nixos.waybar =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        swaynotificationcenter
-      ];
-    };
+  flake.modules = {
+    nixos.waybar =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = with pkgs; [
+          swaynotificationcenter
+        ];
+      };
+    # Autostart if on hyprland
+    homeManager.waybar.wayland.windowManager.hyprland.settings.exec-once = [
+      "swaync"
+    ];
+  };
+
 }
