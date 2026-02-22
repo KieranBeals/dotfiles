1a006c8765d770a364a67edb9a87909a32c3100f
Kieran Beals <kieranbeals@gmail.com>
2026-02-22 02:08:17 -0500

    Created authorizedKeys in my user

--- modules/users/kieran.nix
+++ modules/users/kieran.nix
@@ -3,6 +3,9 @@
     meta.users.kieran = {
       name = "Kieran Beals";
       email = "kieranbeals@gmail.com";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJYE+NOzj9PuzT56bxgxeAlCNMYOVzO12fCEmo8N+7oY kieran@laptop"
+      ];
     };
   };
 }
