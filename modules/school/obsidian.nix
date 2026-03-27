6764b97dec78fe965dd3a56aec0fdc906bcccaff
Kieran Beals <kieranbeals@gmail.com>
2026-03-27 03:39:17 -0400

    Update obsidian.nix

--- modules/school/obsidian.nix
+++ modules/school/obsidian.nix
@@ -8,6 +8,12 @@
         environment.systemPackages = with pkgs; [
           obsidian
         ];
+
+        # Obsidian has not updated?
+        # TODO: Remove
+        nixpkgs.config.permittedInsecurePackages = [
+          "electron-38.8.4"
+        ];
       };
   };
 }
