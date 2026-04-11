a64b3b477d28be6eb3a3a4d2323cf83cda7ba970
Kieran Beals <kieranbeals@gmail.com>
2026-04-11 00:46:47 -0400

    Update file-browser.nix

--- modules/desktop/apps/file-browser.nix
+++ modules/desktop/apps/file-browser.nix
@@ -1,9 +1,20 @@
 {
   flake.modules = {
-    nixos.desktop = {
-      programs.thunar.enable = true;
-      # needed by thundar to do removable media and more
-      services.gvfs.enable = true;
-    };
+    nixos.desktop =
+      { pkgs, ... }:
+      {
+        programs.thunar = {
+          enable = true;
+          plugins = with pkgs.xfce; [
+            thunar-vcs-plugin
+            thunar-archive-plugin
+            thunar-media-tags-plugin
+            thunar-volman
+          ];
+        };
+
+        # needed by thundar to do removable media and more
+        services.gvfs.enable = true;
+      };
   };
 }
