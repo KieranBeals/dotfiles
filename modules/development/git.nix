ea7d58ecc2cea9e920fd50678fc0fc935bee410d
Kieran Beals <kieranbeals@gmail.com>
2026-04-25 14:59:01 -0400

    Update git.nix

--- modules/development/git.nix
+++ modules/development/git.nix
@@ -3,46 +3,54 @@ let
   inherit (config.flake) meta;
 in
 {
-  flake.modules.homeManager.development = {
-    programs = {
-      git = {
-        enable = true;
-        settings = {
-          user = {
-            inherit (meta.users.kieran) email;
-            inherit (meta.users.kieran) name;
-          };
+  flake.modules = {
 
-          alias = {
-            a = "add";
-            b = "branch";
-            c = "commit";
-            ca = "commit --amend";
-            cm = "commit -m";
-            co = "checkout";
-            d = "diff";
-            ds = "diff --staged";
-            l = "log --oneline";
-            ll = "log";
-            p = "push";
-            pf = "push --force-with-lease";
-            pl = "pull";
-            r = "rebase";
-            s = "status --short";
-            ss = "status";
-            sw = "switch";
-            # W alias by Karun
-            forgor = "commit --amend --no-edit";
-            graph = "log --all --decorate --graph --oneline";
-            oops = "checkout --";
+    nixos.development =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [ pkgs.gh ];
+      };
+    homeManager.development = {
+      programs = {
+        git = {
+          enable = true;
+          settings = {
+            user = {
+              inherit (meta.users.kieran) email;
+              inherit (meta.users.kieran) name;
+            };
+
+            alias = {
+              a = "add";
+              b = "branch";
+              c = "commit";
+              ca = "commit --amend";
+              cm = "commit -m";
+              co = "checkout";
+              d = "diff";
+              ds = "diff --staged";
+              l = "log --oneline";
+              ll = "log";
+              p = "push";
+              pf = "push --force-with-lease";
+              pl = "pull";
+              r = "rebase";
+              s = "status --short";
+              ss = "status";
+              sw = "switch";
+              # W alias by Karun
+              forgor = "commit --amend --no-edit";
+              graph = "log --all --decorate --graph --oneline";
+              oops = "checkout --";
+            };
           };
         };
-      };
 
-      # Syntax-highlighting
-      delta = {
-        enable = true;
-        enableGitIntegration = true;
+        # Syntax-highlighting
+        delta = {
+          enable = true;
+          enableGitIntegration = true;
+        };
       };
     };
   };
