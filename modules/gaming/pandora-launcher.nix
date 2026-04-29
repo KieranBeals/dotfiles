dad8ffe41577eddd776704980c55cea0f2556a08
Kieran Beals <kieranbeals@gmail.com>
2026-04-28 23:14:30 -0400

    Update pandora-launcher.nix

--- modules/gaming/pandora-launcher.nix
+++ modules/gaming/pandora-launcher.nix
@@ -5,10 +5,10 @@
     let
       pandora-launcher = pkgs.appimageTools.wrapType1 {
         pname = "pandora-launcher";
-        version = "4.1.0";
+        version = "5.0.2";
         src = pkgs.fetchurl {
-          url = "https://github.com/Moulberry/PandoraLauncher/releases/download/v4.1.0/PandoraLauncher-Linux-x86_64.AppImage";
-          hash = "sha256-20jSeUjxJCpolZA55M6jopMQqeE4xOdprtW+fwGq2Ac=";
+          url = "https://github.com/Moulberry/PandoraLauncher/releases/download/v5.0.2/PandoraLauncher-Linux-x86_64.AppImage";
+          hash = "sha256-I75colvAcDwXBTLT6G9i8VCZzMqn8GACxYt27XXM1HQ=";
         };
       };
     in
