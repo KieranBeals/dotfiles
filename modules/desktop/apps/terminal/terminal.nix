66240cab543bb2ee3650c9f1b6d942db91a43e7b
Kieran Beals <kieranbeals@gmail.com>
2026-04-29 00:12:13 -0400

    Update terminal.nix

--- modules/desktop/apps/terminal/terminal.nix
+++ modules/desktop/apps/terminal/terminal.nix
@@ -8,8 +8,32 @@
         ];
       };
     homeManager.desktop = {
-      home.file.".config/ghostty/config" = {
-        source = ./config;
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "x-scheme-handler/terminal" = "com.mitchellh.ghostty.desktop";
+        };
+      };
+
+      home.file = {
+        ".config/ghostty/config".source = ./config;
+
+        ".config/xfce4/helpers.rc".text = ''
+          TerminalEmulator=ghostty
+        '';
+
+        ".local/share/xfce4/helpers/ghostty.desktop".text = ''
+          [Desktop Entry]
+          NoDisplay=true
+          Version=1.0
+          Encoding=UTF-8
+          Type=X-XFCE-Helper
+          Name=Ghostty
+          X-XFCE-Category=TerminalEmulator
+          X-XFCE-Commands=ghostty --window-inherit-working-directory=false --working-directory=inherit
+          X-XFCE-CommandsWithParameter=ghostty --window-inherit-working-directory=false --working-directory=inherit -e "%s"
+          Icon=com.mitchellh.ghostty
+        '';
       };
     };
   };
